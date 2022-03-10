3f3a072e94d10b74d0f64bb2423692df53d539a9
abrahamseni <aagung@reapit.com>
2022-03-10 15:11:31 +0700

    feat/create contact api

---
+++ src/platform-api/contact-api.ts
@@ -0,0 +1,44 @@
+import axios from '../axios/axios'
+import { useReapitConnect } from '@reapit/connect-session'
+import { useQuery, QueryKey } from 'react-query'
+import qs from 'qs'
+import { URLS } from '../constants/api'
+import { reapitConnectBrowserSession } from '../core/connect-session'
+import { ContactModelPagedResult } from '@reapit/foundations-ts-definitions'
+import isEmpty from 'lodash.isempty'
+
+export type SearchContactParam = {
+  name?: string
+  address?: string
+  identityCheck?: string
+}
+
+const fetchContactsBy = async (params: SearchContactParam) => {
+  try {
+    const result = await axios.get(`${URLS.CONTACTS}/?${qs.stringify(params)}`)
+    if (result.status < 400) {
+      return result.data
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const useFetchContactsBy = (params: SearchContactParam) => {
+  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+  const beginFetch = () => !!(connectSession?.accessToken && !isEmpty(params))
+
+  const contactListResult = useQuery<
+    ContactModelPagedResult,
+    Error,
+    ContactModelPagedResult,
+    SearchContactParam & QueryKey
+  >(['searchContactBy', params], () => fetchContactsBy(params), {
+    enabled: beginFetch(),
+    onError: (error) => {
+      throw new Error(error.message)
+    },
+  })
+
+  return contactListResult
+}
