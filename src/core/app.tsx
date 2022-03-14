241b7fb2aefa067101f7f0c9ad2a9d4dac78708a
Azka Nurfadillah <azka@softwareseni.com>
2022-03-14 08:30:02 +0700

    fix: merging bug

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -5,7 +5,6 @@ import Router from './router'
 import ErrorBoundary from '../components/hocs/error-boundary'
 import { MediaStateProvider, NavStateProvider } from '@reapit/elements'
 import '@reapit/elements/dist/index.css'
-import { QueryClientProvider, QueryClient } from 'react-query'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,8 +13,6 @@ const queryClient = new QueryClient({
     },
   },
 })
-
-const queryClient = new QueryClient()
 
 const App: FC = () => (
   <ErrorBoundary>
