4b4aa330dcdc55ae79fc9c84567b5184774a428e
Dimas M <dimas.m@softwareseni.com>
2022-03-25 08:44:43 +0700

    test: update test file

--- src/components/ui/checklist-details-steps/declaration-risk-management/__tests__/declaration-risk-management.test.tsx
+++ src/components/ui/checklist-details-steps/declaration-risk-management/__tests__/declaration-risk-management.test.tsx
@@ -17,13 +17,6 @@ const axiosMock = new AxiosMockAdapter(Axios, {
 })
 
 jest.unmock('@reapit/connect-session')
-jest.mock('react-pdf/dist/esm/entry.webpack', () => {
-  return {
-    __esModule: true,
-    Document: () => null,
-    Page: () => null,
-  }
-})
 jest.mock('@reapit/elements', () => jest.requireActual('utils/mocks/reapit-element-mocks'))
 jest.mock('core/connect-session')
 jest.mock('components/ui/ui/document-preview-modal', () => {
@@ -203,7 +196,7 @@ describe('Declaration Risk Management Form', () => {
       fireEvent.click(submitButton)
       await wait(0)
 
-      expect(error).toBeCalledTimes(1)
+      expect(error).toBeCalledTimes(2)
       expect(error.mock.calls[0][0]).toMatch(/Failed to submit Declaration Risk Management form/i)
       expect(axiosMock.history.patch[0].url).toEqual('/contacts/MLK16000071')
     })
