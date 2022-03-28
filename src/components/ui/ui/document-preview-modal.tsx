a0d777b293d5fd00e97b2df3513dbc39e2eebc5e
Hanif Rahman <hanif.rahman@softwareseni.com>
2022-03-28 09:44:04 +0700

    ui: change view in new tab button to download button in document preview

--- src/components/ui/ui/document-preview-modal.tsx
+++ src/components/ui/ui/document-preview-modal.tsx
@@ -4,7 +4,7 @@ import Modal from './modal'
 import { modalMaxHeight, modalHeaderHeight, modalBodyPadding } from './__styles__/modal.style'
 import { styled } from '@linaria/react'
 import { css } from '@linaria/core'
-import { isObjectUrl, isSameOrigin } from '../../../utils/url'
+import { isObjectUrl } from '../../../utils/url'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileLines } from '@fortawesome/free-solid-svg-icons'
 
@@ -42,44 +42,46 @@ export const DocumentPreviewModal: FC<DocumentPreviewModalProps> = ({
         overflow-y: hidden;
       `}
     >
-      <Body>
-        {loading && <Loader label="Loading" />}
-        {src &&
-          !loading &&
-          (!pdfPreviewIsVisible ? (
-            <img
-              src={src}
-              onError={() => setPdfPreviewIsVisible(true)}
-              onLoad={() => setImageLoaded(true)}
-              alt={imageLoaded ? 'image preview' : undefined}
-            />
-          ) : (
-            <IconContainer>
-              <Icon icon={faFileLines} />
-            </IconContainer>
-          ))}
-      </Body>
-      <Footer className="el-pt6">
-        <FlexContainer isFlexJustifyEnd>
-          <a
-            className="el-button el-intent-primary el-mr4"
-            href={src}
-            download={filename ? filename : true}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {!src || isSameOrigin(src) ? 'Download' : 'View in new tab'}
-          </a>
-          <Button intent="low" onClick={closeModal}>
-            Close
-          </Button>
-        </FlexContainer>
-        <PersistantNotification isExpanded isFullWidth className="el-mt4">
-          {`If you're having trouble viewing the above image or if it doesn't display correctly,
-            please use the 'Download' option above. Once downloaded, you can select your
-            default browser to view the document (Internet Explorer, Chrome etc)`}
-        </PersistantNotification>
-      </Footer>
+      {loading && <FlexContainer isFlexJustifyCenter><Loader label="Loading" /></FlexContainer>}
+      {src && !loading && (
+        <>
+          <Body>
+            {!pdfPreviewIsVisible ? (
+              <img
+                src={src}
+                onError={() => setPdfPreviewIsVisible(true)}
+                onLoad={() => setImageLoaded(true)}
+                alt={imageLoaded ? 'image preview' : undefined}
+              />
+            ) : (
+              <IconContainer>
+                <Icon icon={faFileLines} />
+              </IconContainer>
+            )}
+          </Body>
+          <Footer className="el-pt6">
+            <FlexContainer isFlexJustifyEnd>
+              <a
+                className="el-button el-intent-primary el-mr4"
+                href={src}
+                download={filename ? filename : true}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download
+              </a>
+              <Button intent="low" onClick={closeModal}>
+                Close
+              </Button>
+            </FlexContainer>
+            <PersistantNotification isExpanded isFullWidth className="el-mt4">
+              {`If you're having trouble viewing the above image or if it doesn't display correctly,
+                please use the 'Download' option above. Once downloaded, you can select your
+                default browser to view the document (Internet Explorer, Chrome etc)`}
+            </PersistantNotification>
+          </Footer>
+        </>
+      )}
     </Modal>
   )
 }
