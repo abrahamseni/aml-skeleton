43acc250fc731c8267cecb6944070a51d027585a
Azka Nurfadillah <azka@softwareseni.com>
2022-03-29 14:45:27 +0700

    chore: update form validation

--- src/components/ui/checklist-details-steps/personal-details/form-schema/validation-schema.ts
+++ src/components/ui/checklist-details-steps/personal-details/form-schema/validation-schema.ts
@@ -2,9 +2,15 @@ import * as Yup from 'yup'
 import { errorMessages } from '../../../../../constants/error-messages'
 
 const validationSchema = Yup.object().shape({
-  title: Yup.string().trim().required(errorMessages.FIELD_REQUIRED),
-  forename: Yup.string().trim().required(errorMessages.FIELD_REQUIRED),
-  surname: Yup.string().trim().required(errorMessages.FIELD_REQUIRED),
+  title: Yup.string().trim().required(errorMessages.FIELD_REQUIRED).max(15, errorMessages.MAXIMUM_CHARACTER_LENGTH(15)),
+  forename: Yup.string()
+    .trim()
+    .required(errorMessages.FIELD_REQUIRED)
+    .max(20, errorMessages.MAXIMUM_CHARACTER_LENGTH(20)),
+  surname: Yup.string()
+    .trim()
+    .required(errorMessages.FIELD_REQUIRED)
+    .max(20, errorMessages.MAXIMUM_CHARACTER_LENGTH(20)),
   dateOfBirth: Yup.string().nullable().required(errorMessages.FIELD_REQUIRED),
   email: Yup.string().trim().email('Please enter a valid email format!').required(errorMessages.FIELD_REQUIRED),
   mobilePhone: Yup.string().nullable(),
